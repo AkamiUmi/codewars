e03bd3ea1014ef28af66a586df8b67fad0cfde7d
akamiumi <akamiumi@gmail.com>
2023-08-11 12:03:17 +0500

    adding kata ling

--- not-very-secure.java
+++ not-very-secure.java
@@ -1,5 +1,5 @@
 public class SecureTester{
-  
+  //https://www.codewars.com/kata/526dbd6c8c0eb53254000110/train/java
     private static boolean check(char s){
       if((s >= 'a' && s <= 'z') ||
         (s >='A' && s <= 'Z') ||
